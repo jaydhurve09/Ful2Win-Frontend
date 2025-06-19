071471abb2cfd2e1645038e3a005dca4505425bc
jaydhurve09 <jaydhurve09@gmail.com>
2025-06-19 11:58:13 +0530

    minor changes

--- src/components/PopularGames.jsx
+++ src/components/PopularGames.jsx
@@ -5,7 +5,7 @@ import carrom from '../assets/carrom.png';
 
 const PopularGames = () => {
   return (
-    <section className="my-12">
+    <section className="my-8">
       <h2 className="text-2xl font-bold mb-4 text-pink-200">⭐ Popular Games</h2>
       <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2">
         <div className="bg-gray-800/50 rounded-lg overflow-hidden shadow-lg hover:shadow-pink-500/20 transition-all duration-300 hover:-translate-y-1">
