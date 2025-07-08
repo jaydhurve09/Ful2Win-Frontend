381a656e6b2daa98323fae937ce4d6a6db9af635
Shravan Pusdekar <shrvaanpusdekar16@gmail.com>
2025-07-08 13:06:00 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
   const isProduction = mode === 'production';
   const apiBaseUrl = env.VITE_API_BACKEND_URL || 'http://localhost:5000';
   
