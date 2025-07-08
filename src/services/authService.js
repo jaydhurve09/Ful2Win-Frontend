7572702f63bb6722aa372080787c8fcae3a51099
Shravan Pusdekar <shrvaanpusdekar16@gmail.com>
2025-07-08 10:39:57 +0530

    authService Fix

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,7 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 const apiBaseUrl = import.meta.env.VITE_API_BACKEND_URL;
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api' || `${apiBaseUrl}/api`,
+  baseURL: apiBaseUrl ? `${apiBaseUrl}/api` : 'http://localhost:5000/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
