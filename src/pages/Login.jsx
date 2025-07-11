c74435c5aaa8c702df1bcac61342aca2725b453d
Shravan Pusdekar <shrvaanpusdekar16@gmail.com>
2025-07-11 18:08:07 +0530

    password manager removed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -101,7 +101,7 @@ const Login = () => {
             <h2 className="text-2xl font-semibold text-gray-700 mb-5">Hello Again!</h2>
             <p className="text-gray-500 mb-5">Login to continue</p>
 
-            <form onSubmit={handleLogin}>
+            <form onSubmit={handleLogin} autoComplete='off'>
               <div className="mb-4">
                 <label className="block text-sm font-medium text-gray-700 mb-1">Phone Number</label>
                 <input
@@ -112,6 +112,7 @@ const Login = () => {
                   className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-blue-300"
                   maxLength="10"
                   required
+                  autoComplete='off'
                 />
               </div>
 
@@ -124,6 +125,7 @@ const Login = () => {
                   placeholder="Enter your password"
                   className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-blue-300"
                   required
+                  autoComplete='new-password'
                 />
               </div>
 
