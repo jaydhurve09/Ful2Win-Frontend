ea07dbe747733c1f14d03ca25917c788fead16e2
Shravan Pusdekar <shrvaanpusdekar16@gmail.com>
2025-07-15 10:42:16 +0530

    done

--- src/services/chatService.js
+++ src/services/chatService.js
@@ -10,7 +10,7 @@ const getAuthHeaders = () => {
   };
 };
 
-const baseUrl = import.meta.env.MODE === 'development' ? 'http://localhost:5000' : import.meta.env.BACKEND_URL;
+const baseUrl = import.meta.env.MODE === 'development' ? 'http://localhost:5000' : import.meta.env.VITE_API_BACKEND_URL;
 
 const chatService = {
   /**
