590daf300d5bcb4271e1eb024058c29270a2346f
Swapnil Pawar <sp1880109@gmail.com>
2025-07-24 14:13:07 +0530

    scoreCard Updated

--- src/App.jsx
+++ src/App.jsx
@@ -111,7 +111,7 @@ function App() {
               <Route path="/gameOn/:gameId/:tournamentId" element={<GameOn />} />
               <Route path="/comingsoon" element={<ComingSoon />} />
               <Route path="/startscreen" element={<StartScreen />} />
-              
+                         
 
               {/* Game routes */}
               
--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -6,8 +6,9 @@ import React, { useEffect } from "react";
 import confetti from 'canvas-confetti';
 import { MdOutlineReplay } from "react-icons/md";
 import { IoHomeOutline } from "react-icons/io5";
-
-const ScoreCard = ({ game, userId, score, roomId, gameName }) => {
+import { motion } from "framer-motion";
+import logo from '../assets/logo.jpg' // Default image if gameImg is not provided
+const ScoreCard = ({ game, userId, score, roomId, gameName, gameImg }) => {
   const navigate = useNavigate();
 
    
@@ -27,51 +28,53 @@ const ScoreCard = ({ game, userId, score, roomId, gameName }) => {
 //sound effect added for win..
 
   return (
-   <div className="w-screen h-screen blur-0 text-yellow-400 bg-blueGradient flex items-center justify-center">
-  <div className="w-full max-w-sm bg-transparent backdrop-blur-md rounded-2xl shadow-xl p-6 relative border-2 shadow-gray-400 border-yellow-500">
+   <div    className="w-screen h-screen blur-none text-yellow-400 bg-black flex items-center justify-center">
+  <motion.div initial={{ scale: 0 }} delay={1.6} animate={{ scale: 1 }} className="w-full max-w-sm bg-gray-900 rounded-2xl shadow-md p-6 relative border-2 shadow-gray-400 border-yellow-500 m-3">
         
         {/* Cancel Button */}
         <button
           onClick={() => navigate(-1)}
-          className="absolute top-4 right-4 text-yellow-400 hover:text-yellow-300 text-xl transition-all"
+          className="absolute top-4 right-4  text-white hover:text-yellow-300 text-xl transition-all"
           title="Back to Lobby"
         >
           <ImCancelCircle />
         </button>
 
         {/* Header */}
-        <div className="text-center mb-6">
-          <h2 className="text-2xl font-extrabold">Your Score</h2>
+        <div className="text-center flex flex-row items-center mb-6">
+          <img className="w-16 h-16 rounded-full m-4" src={gameImg||logo} alt={gameName} /> <h2 className="text-2xl font-extrabold">{gameName || 'Unknown Game'}</h2>
         </div>
-
+      
+        
+      
         {/* Score Display */}
-        <div className="bg-yellow-100 text-yellow-800 rounded-xl py-2 flex  justify-between items-center mb-6 text-center shadow-inner">
-          <div className="flex justify-center items-center mb-2 text-4xl pl-1 font-bold">
-            <FaTrophy className="mr-4 " /> {score}
+        <div className=" text-yellow-800 rounded-xl py-2 flex  justify-center items-center mb-6 text-center shadow-inner">
+          <div className="flex justify-center gap-3 items-center mb-2 text-4xl pl-1 font-bold">
+           <h2 className="text-yellow-400">My Score:</h2> <p className="text-4xl font-bold text-white"> 50{score}</p>
           </div>
           
-          <p className="text-sm flex items-bottom text-gray-800 mt-4 p-1">Your Score in{gameName}</p>
+         
           
         </div>
 
         {/* Details Section */}
       
         {/* Actions */}
-        <div className="mt-6 flex flex-col items-center gap-4">
+        <div className="mt-6 flex flex-row items-center justify-evenly gap-4">
           <button
             onClick={() => navigate( `/tournament-lobby/${game}`)}
-            className="w-full bg-yellow-400 hover:bg-yellow-500 text-blue-900 font-bold py-2 px-6 rounded-xl shadow transition-all duration-200 flex items-center justify-center gap-2"
+            className="w-full bg-gradient-to-r from-purple-600 to-blue-600 hover:bg-yellow-500 text-white text-bold font-bold py-2 px-6 rounded-xl shadow transition-all duration-200 flex items-center justify-center gap-2"
           >
-            <MdOutlineReplay  className="text-xl font-semibold" /> Play Again
+            Play Again
           </button>
           <button
             onClick={() => navigate("/tournaments")}
-            className="w-full bg-white text-yellow-500 hover:bg-gray-100 font-semibold py-2 px-6 rounded-xl transition-all duration-200 border border-blue-300 flex items-center justify-center gap-2"
+            className="w-full bg-gray-500 text-white hover:bg-gray-100  font-semibold p-2 py-2 px-6 rounded-xl transition-all duration-200 border border-blue-300 flex items-center justify-center gap-2"
           >
-            <IoHomeOutline className="text-xl font-semibold" /> Back to Tournament
+            Tournaments
           </button>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
